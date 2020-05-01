60b222d567cda0132107ec83c3b3d7a253b47d7a
Dmitry Makarevich <>
2020-05-01 10:59:59 +0300

    add category models

---
+++ MovieWorld/Common/Models/Category/MWCategory.swift
@@ -0,0 +1,39 @@
+//
+//  MWCategory.swift
+//  MovieWorld
+//
+//  Created by Admin on 11.04.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+
+class MWCategory: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+    }
+    
+    let id: Int
+    let name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+        self.id = (try? container.decode(Int.self, forKey: .id)) ?? 0
+        self.name = (try? container.decode(String.self, forKey: .name)) ?? Constants.emptyString
+    }
+        
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+            
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+    }        
+}
---
+++ MovieWorld/Common/Models/Category/MWResponseCategory.swift
@@ -0,0 +1,18 @@
+//
+//  MWResponse.swift
+//  MovieWorld
+//
+//  Created by Admin on 29.04.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+
+struct MWResponseCategory: Codable {
+    let genres: [MWCategory]
+    
+    enum CodingKeys: String, CodingKey {
+      case genres
+    }
+}
