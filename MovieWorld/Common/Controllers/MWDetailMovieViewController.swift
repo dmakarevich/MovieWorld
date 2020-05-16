049780a1174fecde898323ff1c7a8a0650b9fb4b
Dmitry Makarevich <>
2020-05-16 11:58:55 +0300

    add uicollectionview with movie cast to detail movie controller

--- MovieWorld/Common/Controllers/MWDetailMovieViewController.swift
+++ MovieWorld/Common/Controllers/MWDetailMovieViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class MWDetailMovieViewController: MWViewController {
     //MARK: - Variables
+    private let movieCardEdges = UIEdgeInsets(top: 16, left: 0, bottom: 12, right: 0)
+    private let descriptionBlockEdges = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    private let movieCrewHeaderEdges = UIEdgeInsets(top: 24, left: 16, bottom: 12, right: 26)
+    private let collectionViewEdges = UIEdgeInsets(top: 4, left: 16, bottom: 12, right: 0)
+
     private var detailMovie: MWDetailMovie?
+    private var movieCrew: MWMovieCrew?
+
+    // MARK: - Gui variables
+    private let contentView = UIView()
     private var movieCard = MWMovieCardView()
     private var descriptionBlock = MWDescriptionView()
     private var movieCrewHeader = MWHeaderView()
@@ -19,12 +28,29 @@ class MWDetailMovieViewController: MWViewController {
         let scrollView = UIScrollView()
         return scrollView
     }()
-    private let movieCardEdges = UIEdgeInsets(top: 16, left: 0, bottom: 12, right: 0)
-    private let descriptionBlockEdges = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    private let movieCrewHeaderEdges = UIEdgeInsets(top: 24, left: 16, bottom: 12, right: 26)
 
-    private let contentView = UIView()
+    //MARK: - Gui variables
+    private lazy var flowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = CGSize(width: 72, height: 130)
+        flowLayout.minimumInteritemSpacing = 16.0
+        flowLayout.sectionInset.bottom = 5
 
+        return flowLayout
+    } ()
+
+    private lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: self.flowLayout)
+        cv.dataSource = self
+        cv.delegate = self
+        cv.register(MWCastCollectionViewCell.self, forCellWithReuseIdentifier: MWCastCollectionViewCell.cellReuseIdentifier)
+        cv.backgroundColor = .white
+
+        return cv
+    } ()
+
+    //MARK: - Add constraints
     func makeConstraints() {
         self.scrollView.snp.makeConstraints({ (make) in
             make.centerX.equalToSuperview()
@@ -49,7 +75,13 @@ class MWDetailMovieViewController: MWViewController {
         self.movieCrewHeader.snp.makeConstraints({ (make) in
             make.top.equalTo(self.descriptionBlock.snp.bottom)
             make.left.right.equalTo(self.contentView).inset(self.movieCrewHeaderEdges)
-            make.bottom.lessThanOrEqualTo(self.contentView.snp.bottom).inset(self.descriptionBlockEdges.bottom)
+        })
+
+        self.collectionView.snp.makeConstraints({ (make) in
+            make.top.equalTo(self.movieCrewHeader.snp.bottom).offset(self.collectionViewEdges.top)
+            make.left.right.equalToSuperview().inset(self.collectionViewEdges)
+            make.height.equalTo(155)
+            make.bottom.lessThanOrEqualTo(self.contentView.snp.bottom).inset(self.collectionViewEdges.bottom)
         })
     }
 
@@ -65,13 +97,16 @@ class MWDetailMovieViewController: MWViewController {
         self.contentView.addSubview(self.movieCard)
         self.contentView.addSubview(self.descriptionBlock)
         self.contentView.addSubview(self.movieCrewHeader)
+        self.contentView.addSubview(self.collectionView)
         self.movieCrewHeader.set(title: "Cast")
     }
 
     func setData() {
         if let movie = self.detailMovie {
             self.movieCard.setData(movie: movie)
-            self.descriptionBlock.setData(description: movie.description, runtime: movie.getRuntime(), isAdult: movie.adult)
+            self.descriptionBlock.setData(description: movie.description,
+                                          runtime: movie.getRuntime(),
+                                          isAdult: movie.adult)
         }
 
         self.makeConstraints()
@@ -83,35 +118,108 @@ class MWDetailMovieViewController: MWViewController {
             guard let data = data else { return }
             self.detailMovie?.image = data
 
-            self.setData()
+            self.fetchMovieCredits(movieId: model.id)
         }
 
-        MWNet.sh.getImage(imagePath: model.poster,
+        MWNet.sh.requestImage(imagePath: model.poster,
                           size: .w154,
                           handler: handler)
     }
 
     func getMovie(movieId: Int) {
-        let success: SuccessHandler = { [weak self] (response: MWDetailMovie) in
+        let success: SuccessHandler = { [weak self] (movie: MWDetailMovie) in
             guard let self = self else { return }
 
-            debugPrint("~~~~~~~~~~~~~~~~~~~~~")
-            debugPrint("backdropPath = \(response.backdropPath)")
-            debugPrint("poster = \(response.poster)")
-            debugPrint("~~~~~~~~~~~~~~~~~~~~~")
             if let sizes = MWSys.sh.configuration?.posterSizes {
                 sizes.forEach({ (size) in
                     debugPrint(size.rawValue)
                 })
             }
-            self.getImage(model: response)
+            self.getImage(model: movie)
+            self.fetchMovieCredits(movieId: movie.id)
+        }
+
+        let errors = { (error: MWNetError) in
+            print(error)
         }
 
         let url: String = URLPaths.movieById + String(movieId)
 
-        MWNet.sh.request(urlPath: url,
-                         queryParameters: ["language": URLLanguage.by.urlValue],
-                         of: MWDetailMovie.self,
-                         successHandler: success)
+        MWNet.sh.requestAlamofire(url: url,
+                         parameters: ["language": URLLanguage.by.urlValue],
+                         successHandler: success,
+                         errorHandler: errors)
+    }
+
+    func fetchMovieCredits(movieId: Int) {
+        let success: SuccessHandler = { [weak self] (movieCrew: MWMovieCrew) in
+            guard let self = self else { return }
+            self.getCastImage(crew: movieCrew)
+        }
+
+        let errors = { (error: MWNetError) in
+            print(error)
+        }
+        let url = "/movie/\(movieId)/credits"
+        let params = ["language": URLLanguage.by.urlValue]
+
+        MWNet.sh.requestAlamofire(url: url,
+                         parameters: params,
+                         successHandler: success,
+                         errorHandler: errors)
+    }
+
+    func getCastImage(crew: MWMovieCrew) {
+        let dispatch = DispatchGroup()
+        crew.cast.forEach ({ (person) in
+            let completion: CompetionImageHandler = { (data) in
+                person.image = data
+                dispatch.leave()
+            }
+
+            guard let path = person.profilePath else { return }
+            dispatch.enter()
+            MWNet.sh.requestImage(imagePath: path,
+                              size: .w92,
+                              handler: completion)
+        })
+
+        dispatch.notify(queue: .main) {
+            self.movieCrew = crew
+
+            self.setData()
+            self.collectionView.reloadData()
+        }
+    }
+}
+
+//MARK: - Collection view Datasource and Delegate Methods
+extension MWDetailMovieViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.movieCrew?.cast.count ?? 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MWCastCollectionViewCell.cellReuseIdentifier, for: indexPath)
+
+        if let person = self.movieCrew?.cast[indexPath.row] {
+            (cell as? MWCastCollectionViewCell)?.set(person: person)
+        }
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+//        let cell = collectionView.cellForItem(at: indexPath) as? MWCastCollectionViewCell
+        let moviesInSection = MWDetailMovieViewController()
+        moviesInSection.view.backgroundColor = .white
+
+//        if let id = cell?.movieId {
+//            moviesInSection.getMovie(movieId: id)
+//
+//            MWI.sh.push(vc: moviesInSection)
+//        }
     }
 }
---
+++ MovieWorld/Features/Main/CollectionViewCells/MWCastCollectionViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  MWCastCollectionViewCell.swift
+//  MovieWorld
+//
+//  Created by Admin on 16.05.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class MWCastCollectionViewCell: UICollectionViewCell {
+    //MARK: - Variables
+    static var cellReuseIdentifier: String = "MWCastCollectionViewCell"
+
+    private let imageSize = CGSize(width: 72, height: 92)
+    private let coverImageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0)
+    private let titleEdgeInsets = UIEdgeInsets(top: 12, left: 0, bottom: 0, right: 0)
+
+    //MARK: - Gui variables
+    private var coverImage: UIImageView = {
+        let iv = UIImageView()
+        iv.cornerRadius = 5
+        iv.image = UIImage(named: Constants.Images.defaultCard)
+
+        return iv
+    }()
+
+    private var title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: Constants.FontSize.size17, weight: .bold)
+        label.textColor = UIColor.init(named: Constants.Colors.textColor)
+        label.text = "Name"
+
+        return label
+    }()
+
+    private var subtitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: Constants.FontSize.size13)
+        label.textColor = .black
+        label.text = "Character"
+
+        return label
+    }()
+
+    //MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.contentView.addSubview(self.coverImage)
+        self.contentView.addSubview(self.title)
+        self.contentView.addSubview(self.subtitle)
+
+        self.updateConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //MARK: - Constraints
+    override func updateConstraints() {
+        self.coverImage.snp.updateConstraints { (make) in
+            make.top.left.right.equalToSuperview().inset(self.coverImageEdgeInsets)
+            make.size.equalTo(self.imageSize)
+        }
+
+        self.title.snp.updateConstraints { (make) in
+            make.top.equalTo(self.coverImage.snp.bottom)
+                .offset(self.titleEdgeInsets.top)
+            make.left.right.equalToSuperview()
+                .offset(self.titleEdgeInsets.left)
+        }
+
+        self.subtitle.snp.updateConstraints { (make) in
+            make.top.equalTo(self.title.snp.bottom)
+            make.left.right.equalToSuperview()
+            make.bottom.lessThanOrEqualToSuperview().offset(5)
+        }
+
+        super.updateConstraints()
+    }
+
+    func set(person: MWCast) {
+        if let imageData = person.image, let image = UIImage(data: imageData) {
+            self.coverImage.image = image
+        }
+        self.title.text = person.name
+        self.subtitle.text = person.character
+
+        self.setNeedsUpdateConstraints()
+    }
+}
